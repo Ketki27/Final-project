eb65044615e6b4a1c4d2408bd1d4d27c108689e5
Ketki Joshi <ketki.vinayak.joshi@emory.edu>
2023-08-26 22:39:15 -0400

    Regression

--- Final project_R script.R
+++ Final project_R script.R
@@ -1,7 +1,24 @@
 library(gtsummary)
+library(dplyr)
+library(tidyverse)
+
 names(covid_testing)
 
 tbl_summary(
 	covid_testing,
 	include = c(gender, age, result, pan_day, drive_thru_ind, ct_result, payor_group, patient_class, orderset, col_rec_tat, rec_ver_tat))
+
+tbl_uvregression (covid_testing,
+	y = drive_thru_ind,
+	include = c(age, orderset
+							),
+	method = lm)
+
+tbl_uvregression(
+	covid_testing,
+	y = drive_thru_ind,
+	include = c(age, orderset),
+	method = glm,
+	method.args = list(family = binomial()),
+	exponentiate = TRUE)
 
